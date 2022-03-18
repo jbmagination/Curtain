2a12d74ba3f65e9c205e1ff8ba9b69232e6d022c
JBMagination <jbmagination@jbmagination.com>
2022-03-17 20:41:51 -0400

    Update curtain.dev.js

--- curtain.dev.js
+++ curtain.dev.js
@@ -8,63 +8,57 @@ bugs. Use `curtain.min.js` instead. If it doesn't exist, then I don't
 know how you're here; this repository should be private.
 */
 
-class Curtain { 
-  constructor(url, options) {
-    this.url = new URL(url);
-    const loadedUrl = this.url;
+export function Curtain(url, options) {
+  this.url = new URL(url);
+  const loadedUrl = this.url;
     
-    // set default options
-    this.options.httpsUpgrade = options.httpsUpgrade || true;
-    this.options.iframeOptions = options.iframeOptions || {"referrerpolicy":"no-referrer"};
-    this.options.iframeOptions.referrerpolicy = options.iframeOptions.referrerpolicy || "no-referrer";
-    this.options.youtubeBypass = options.youtubeBypass || "nocookie";
-    this.options.invidiousInstance = options.invidiousInstance || "invidious.io";
-    this.options.pipedInstance = options.invidiousInstance || "piped.kavin.rocks";
+  // set default options
+  this.options.httpsUpgrade = options.httpsUpgrade || true;
+  this.options.iframeOptions = options.iframeOptions || {"referrerpolicy":"no-referrer"};
+  this.options.iframeOptions.referrerpolicy = options.iframeOptions.referrerpolicy || "no-referrer";
+  this.options.youtubeBypass = options.youtubeBypass || "nocookie";
+  this.options.invidiousInstance = options.invidiousInstance || "invidious.io";
+  this.options.pipedInstance = options.invidiousInstance || "piped.kavin.rocks";
     
-    if ((this.url.protocol == 'http:') && (this.options.httpsUpgrade == true)) { loadedUrl.protocol = 'https:'; };
-    if (((this.url.hostname == "youtube.com") || (this.url.hostname == "www.youtube.com"))) {
-      if (
-        (this.url.pathname.startsWith('/shorts')) || 
-        (this.url.pathname.startsWith('/watch')) || 
-        (this.url.pathname.startsWith('/v/')
-      ) {
-        const params = new URLSearchParams(this.url.search)
-        if (this.url.pathname.startsWith('/watch')) { 
-          videoId = params.get('v')
-        } else {
-          videoId = this.url.pathname.split('/')[1]
-        }
-        loadedUrl.pathname = '/embed/' + videoId
-        for (const param of params) {
-            if (param.split(',')[0] == 'v') { return }
-            if (loadedUrl.search == '') {
-              loadedUrl.search = '?' + param.split(',').join('=')
-            } else {
-              loadedUrl.search = loadedUrl.search + '&' + param.split(',').join('=')
-            }
+  if ((this.url.protocol == 'http:') && (this.options.httpsUpgrade == true)) { loadedUrl.protocol = 'https:'; };
+  if (((this.url.hostname == "youtube.com") || (this.url.hostname == "www.youtube.com"))) {
+    if ((this.url.pathname.startsWith('/shorts')) || (this.url.pathname.startsWith('/watch')) || (this.url.pathname.startsWith('/v/')) {
+      const params = new URLSearchParams(this.url.search)
+      if (this.url.pathname.startsWith('/watch')) { 
+        videoId = params.get('v')
+      } else {
+        videoId = this.url.pathname.split('/')[1]
+      }
+      loadedUrl.pathname = '/embed/' + videoId
+      for (const param of params) {
+          if (param.split(',')[0] == 'v') { return }
+          if (loadedUrl.search == '') {
+            loadedUrl.search = '?' + param.split(',').join('=')
+          } else {
+            loadedUrl.search = loadedUrl.search + '&' + param.split(',').join('=')
           }
-      loadedUrl.search = loadedUrl.search + '&autoplay=1'
-      }
-      switch (this.options.youtubeBypass) {
-        case "none":
-          loadedUrl.hostname = this.url.hostname;
-          break;
-        case false:
-          loadedUrl.hostname = this.url.hostname;
-          break;
-        case "invidious":
-          loadedUrl.hostname = this.options.invidiousInstance;
-          break;
-        case "piped":
-          loadedUrl.hostname = this.options.pipedInstance;
-          break;
-        default:
-          loadedUrl.hostname = "youtube-nocookie.com";
-      }
-      this.options.iframeOptions.autoplay = options.iframeOptions.autoplay || 1;
+        }
     }
-    console.log(loadedUrl)
+    switch (this.options.youtubeBypass) {
+      case "none":
+        loadedUrl.hostname = this.url.hostname;
+        break;
+      case false:
+        loadedUrl.hostname = this.url.hostname;
+        break;
+      case "invidious":
+        loadedUrl.hostname = this.options.invidiousInstance;
+        break;
+      case "piped":
+        loadedUrl.hostname = this.options.pipedInstance;
+        break;
+      default:
+        loadedUrl.hostname = "youtube-nocookie.com";
+    }
+    this.options.iframeOptions.autoplay = options.iframeOptions.autoplay || 1;
+    loadedUrl.search = loadedUrl.search + '&autoplay=1'
   }
+  console.log(loadedUrl)
 }
 
 // @license-end
